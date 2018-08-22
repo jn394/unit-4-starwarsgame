1867d6893b47cac4267837ff59a66d5ea82013e6
unknown <jayng168@gmail.com>
2018-08-22 11:02:53 -0400

    Updated

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -13,12 +13,17 @@ $(document).ready(function () {
     var attackSidi = 0;
     var attackMaul = 0;
 
+    var startAttackObi = 8;
+    var startAttackLuke = 5;
+    var startAttackSidi = 20;
+    var startAttackMaul = 25;
+
     $("#Obi-Health").text(healthObi);
     $("#Luke-Health").text(healthLuke);
     $("#Sidi-Health").text(healthSidi);
     $("#Maul-Health").text(healthMaul);
 
-    //When Obi-Wan is clicked, i looks at where Obi-Wan is located and then does the following when its in different places.
+    //When Obi-Wan is clicked, it looks at where Obi-Wan is located and then appends Obi-Wan in different places accordingly.
     $("#Obi-Wan").on("click", function () {
         var option1 = $.contains(document.getElementById("characters"), document.getElementById("Obi-Wan"));
         var option2 = $.contains(document.getElementById("enemies"), document.getElementById("Obi-Wan"));
@@ -33,7 +38,7 @@ $(document).ready(function () {
         };
     });
 
-    //When Luke is clicked, i looks at where Luke is located and then does the following when its in different places.
+    //When Luke is clicked, it looks at where Obi-Wan is located and then appends Luke in different places accordingly.
     $("#Luke").on("click", function () {
         var option1 = $.contains(document.getElementById("characters"), document.getElementById("Luke"));
         var option2 = $.contains(document.getElementById("enemies"), document.getElementById("Luke"));
@@ -48,7 +53,7 @@ $(document).ready(function () {
         };
     });
 
-    //When Sidi is clicked, i looks at where Sidi is located and then does the following when its in different places.
+    //When Sidi is clicked, it looks at where Obi-Wan is located and then appends Sidi in different places accordingly.
     $("#Sidi").on("click", function () {
         var option1 = $.contains(document.getElementById("characters"), document.getElementById("Sidi"));
         var option2 = $.contains(document.getElementById("enemies"), document.getElementById("Sidi"));
@@ -63,7 +68,7 @@ $(document).ready(function () {
         };
     });
 
-    //When Maul is clicked, i looks at where Maul is located and then does the following when its in different places.
+    //When Maul is clicked, it looks at where Obi-Wan is located and then appends Maul in different places accordingly.
     $("#Maul").on("click", function () {
         var option1 = $.contains(document.getElementById("characters"), document.getElementById("Maul"));
         var option2 = $.contains(document.getElementById("enemies"), document.getElementById("Maul"));
@@ -80,28 +85,53 @@ $(document).ready(function () {
 
 
 
+
+    function defeatObi() {
+        if (healthObi <= 0) {
+            $("#myEnemy2").empty();
+            $("#myResult").text("Congrates! You have defeated Obi-Wan Kenobi");
+        };
+    };
+
+    function defeatLuke() {
+        if (healthLuke <= 0) {
+            $("#myEnemy2").empty();
+            $("#myResult").text("Congrates! You have defeated Luke Skywalker");
+        };
+    };
+
+
+
+
+
+
+
     //When the Attack button is clicked the health of the enemy is lowered and your characters health is lowered as well
     $("#myAttack").on("click", function () {
         var myObiAttacking = $.contains(document.getElementById("myCharacter2"), document.getElementById("Obi-Wan"));
         var myLukeAttacking = $.contains(document.getElementById("myCharacter2"), document.getElementById("Luke"));
         var mySidiAttacking = $.contains(document.getElementById("myCharacter2"), document.getElementById("Sidi"));
         var myMaulAttacking = $.contains(document.getElementById("myCharacter2"), document.getElementById("Maul"));
-
-
+        
         if (myObiAttacking) {
             attackObi += 8;
             $("#myDamage").text("You attacked for: " + attackObi + " damage");
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Luke"))) {
                 healthLuke -= attackObi;
+                
                 if (healthLuke <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Luke Skywalker");
+                }
+                else {
+                healthObi -= startAttackLuke;
                 };
             };
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Sidi"))) {
                 healthSidi -= attackObi;
+                healthObi -= startAttackSidi;
                 if (healthSidi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Sidious");
@@ -110,12 +140,13 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Maul"))) {
                 healthMaul -= attackObi;
+                healthObi -= startAttackMaul;
                 if (healthMaul <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Maul");
                 };
             };
-        };
+        }
 
         if (myLukeAttacking) {
             attackLuke += 5;
@@ -123,6 +154,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Obi-Wan"))) {
                 healthObi -= attackLuke;
+                healthLuke -= startAttackObi;
                 if (healthObi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Obi-Wan Kenobi");
@@ -131,6 +163,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Sidi"))) {
                 healthSidi -= attackLuke;
+                healthLuke -= startAttackSidi;
                 if (healthSidi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Sidious");
@@ -139,6 +172,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Maul"))) {
                 healthMaul -= attackLuke;
+                healthLuke -= startAttackMaul;
                 if (healthMaul <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Maul");
@@ -152,6 +186,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Obi-Wan"))) {
                 healthObi -= attackSidi;
+                healthSidi -= startAttackObi;
                 if (healthObi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Obi-Wan Kenobi");
@@ -160,6 +195,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Luke"))) {
                 healthLuke -= attackSidi;
+                healthSidi -= startAttackLuke;
                 if (healthLuke <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Luke Skywalker");
@@ -168,6 +204,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Maul"))) {
                 healthMaul -= attackSidi;
+                healthSidi -= startAttackMaul;
                 if (healthMaul <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Maul");
@@ -181,6 +218,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Obi-Wan"))) {
                 healthObi -= attackMaul;
+                healthMaul -= startAttackObi;
                 if (healthObi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Obi-Wan Kenobi");
@@ -189,6 +227,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Luke"))) {
                 healthLuke -= attackMaul;
+                healthMaul -= startAttackLuke;
                 if (healthLuke <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Luke Skywalker");
@@ -197,6 +236,7 @@ $(document).ready(function () {
 
             if ($.contains(document.getElementById("myEnemy2"), document.getElementById("Sidi"))) {
                 healthSidi -= attackMaul;
+                healthMaul -= startAttackSidi;
                 if (healthSidi <= 0) {
                     $("#myEnemy2").empty();
                     $("#myResult").text("Congrates! You have defeated Darth Sidious");
@@ -204,6 +244,9 @@ $(document).ready(function () {
             };
         };
 
+        if (noAttack && noAttack2) {
+            $("#myResults").text("No enemy here")
+        };
 
         $("#Obi-Health").text(healthObi);
         $("#Luke-Health").text(healthLuke);
